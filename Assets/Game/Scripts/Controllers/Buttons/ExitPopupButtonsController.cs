8f14501b64e93b9b333bd5fee73fac7cbe7a877c
Milos-Sstanojevic <milosstanojevic2201@gmail.com>
2026-04-27 02:10:05 +0200

    Itch.io exit

--- Assets/Game/Scripts/Controllers/Buttons/ExitPopupButtonsController.cs
+++ Assets/Game/Scripts/Controllers/Buttons/ExitPopupButtonsController.cs
@@ -1,12 +1,22 @@
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 namespace Game
 {
     public sealed class ExitPopupButtonsController : MonoBehaviour
     {
+#if UNITY_WEBGL && !UNITY_EDITOR
+        [DllImport("__Internal")]
+        private static extern void ReloadItchPage();
+#endif
+
         public void Exit()
         {
+#if UNITY_WEBGL && !UNITY_EDITOR
+            ReloadItchPage();
+#else
             Application.Quit();
+#endif
         }
     }
 }
---
+++ Assets/Plugins/WebGL/ItchInterop.jslib
@@ -0,0 +1,5 @@
+mergeInto(LibraryManager.library, {
+  ReloadItchPage: function () {
+    window.location.reload();
+  }
+});
